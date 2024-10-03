7edcd378750f3e25a2cd0e139a7b0071a1074d82
ALPHABANDIT2009 <will.camp.alpha@gmail.com>
2024-10-02 19:15:11 -0600

    Added Tabs and Movement

---
+++ index_scripts.js
@@ -0,0 +1,9 @@
+// Wait for site to load
+window.addEventListener('load', function () {
+
+
+    // Draggable elements
+    function dragElement(e){var t=0,n=0,o=0,d=0;function l(l){(l=l||window.event).preventDefault(),t=o-l.clientX,n=d-l.clientY,o=l.clientX,d=l.clientY,e.style.top=e.offsetTop-n+"px",e.style.left=e.offsetLeft-t+"px"}function m(){document.onmouseup=null,document.onmousemove=null,document.getElementById("window_IDTITLE").classList.remove("Dragged_State"),document.getElementById("window_tab_IDTITLE").classList.remove("Dragged_State")}document.getElementById("window_tab_IDTITLE").onmousedown=function(e){(e=e||window.event).preventDefault(),o=e.clientX,d=e.clientY,document.onmouseup=m,document.getElementById("window_IDTITLE").classList.add("Dragged_State"),document.getElementById("window_tab_IDTITLE").classList.add("Dragged_State"),document.onmousemove=l}}dragElement(document.getElementById("window_IDTITLE"));
+    function dragElement2(e){var t=0,n=0,o=0,d=0;function l(l){(l=l||window.event).preventDefault(),t=o-l.clientX,n=d-l.clientY,o=l.clientX,d=l.clientY,e.style.top=e.offsetTop-n+"px",e.style.left=e.offsetLeft-t+"px"}function m(){document.onmouseup=null,document.onmousemove=null,document.getElementById("window_IDSUBTITLE").classList.remove("Dragged_State"),document.getElementById("window_tab_IDSUBTITLE").classList.remove("Dragged_State")}document.getElementById("window_tab_IDSUBTITLE").onmousedown=function(e){(e=e||window.event).preventDefault(),o=e.clientX,d=e.clientY,document.onmouseup=m,document.getElementById("window_IDSUBTITLE").classList.add("Dragged_State"),document.getElementById("window_tab_IDSUBTITLE").classList.add("Dragged_State"),document.onmousemove=l}}dragElement2(document.getElementById("window_IDSUBTITLE"));
+
+})
